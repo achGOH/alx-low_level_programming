db8fda557c9b71cc78b8a65e7d9acfab8cd7c6c9
achGOH <achrafbruyne@gmail.com>
2023-06-20 16:56:07 -0700

    ana fas

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - Entry point of the program
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i = 0;
+    
+    positive_or_negative(i);
+    
+    return 0;
 }
